5e2ff2972a26e3b6fcf9f58e648b8e6ca57abdf7
Yuki Tanaka <j1307099@yahoo.co.jp>
2022-05-16 17:44:47 +0900

    extract test data

--- test/utils.js
+++ test/utils.js
@@ -1,4 +1,7 @@
 const testTransferTx = require('./data/checkTransferTx.json');
+const testTx = require('./data/tx.json');
+const testBlockHeader = require('./data/blockHeader.json');
+const testProof = require('./data/proof.json');
 
 class TestData {
   constructor(_accounts) {
@@ -7,6 +10,17 @@ class TestData {
 
   getTransferTxData(index) {
     return testTransferTx[index];
+  }
+  getTxData(index) {
+    return testTx[index];
+  }
+
+  getBlockHeaderData(index) {
+    return testBlockHeader[index];
+  }
+
+  getProofData(index) {
+    return testProof[index];
   }
 
 }
